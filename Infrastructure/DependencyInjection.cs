ce36b014c6524b723a380f006769a705e0074581
RMPV09 <mauupvillegas2000@hotmail.com>
2022-07-26 21:35:33 -0600

    Add project files.

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services, string connectionString)
+        {
+          
+            return services;
+        }
+    }
+}
